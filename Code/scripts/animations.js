e479333e3aa5c264831b554e6860b264c5daa2e1
BuildTools <unconfigured@null.spigotmc.org>
2019-11-26 20:57:43 +0800

    navbar UI change

--- Code/scripts/animations.js
+++ Code/scripts/animations.js
@@ -1,9 +1,52 @@
 var sideElementNumbers = {
-    1: "-300px",
-    2: "-200px",
-    3: "-100px",
-}
-var currentPageSideElement = 0 // 0 is null value, -1 is last element, 1 and onwards is as logical
+    1: "-178px",
+    2: "-118px",
+    3: "-58px",
+};
+var currentPageSideElement = 0; // 0 is null value, -1 is last element, 1 and onwards is as logical
+var highlightedSidenavElementOpacity = false;
+var sidebarExpanded = false;
+
+function sidebarExpand() {
+    $(".sidenav, #sidenavElement-1").stop().animate({
+        "width": "140px"
+    }, 250);
+    if (highlightedSidenavElementOpacity == true) {
+        $("#highlightedSidenavElement").stop().animate({
+            "marginLeft": "108px",
+            "opacity": "1"
+        }, 200);
+    } else {
+        $("#highlightedSidenavElement").stop().animate({
+            "marginLeft": "108px"
+        }, 200);
+    };
+    $("#selectedSidenavElement").stop().animate({
+        "marginLeft": "108px"
+    }, 200);
+    $(".sidenavText").stop().animate({
+        "opacity": "1"
+    }, 200);
+    highlightedSidenavElementOpacity = false;
+    sidebarExpanded = true;
+};
+
+function sidebarCollapse() {
+    $(".sidenav, #sidenavElement-1").stop().animate({
+        "width": "65px"
+    }, 250);
+    $("#highlightedSidenavElement").stop().animate({
+        "opacity": "0",
+        "marginLeft": "33px"
+    }, 200);
+    $("#selectedSidenavElement").stop().animate({
+        "marginLeft": "33px"
+    }, 200);
+    $(".sidenavText").stop().animate({
+        "opacity": "0"
+    }, 200);
+    sidebarExpanded = false;
+};
 
 $(document).ready(function() {
     // just to test if nothing goes wrong
@@ -18,66 +61,80 @@ $(document).ready(function() {
     if (plansSideBar.includes(file)) {
         $("#selectedSidenavElement").stop().show(0, function() {});
         $("#selectedSidenavElement").css({
-            "marginTop": "-300px"
+            "marginTop": "-178px"
         });
         currentPageSideElement = 1
     } else if (fleetSideBar.includes(file)) {
         $("#selectedSidenavElement").stop().show(0, function() {});
         $("#selectedSidenavElement").css({
-            "marginTop": "-200px"
+            "marginTop": "-118px"
         });
         currentPageSideElement = 2
     } else if (checklistSideBar.includes(file)) {
         $("#selectedSidenavElement").stop().show(0, function() {});
         $("#selectedSidenavElement").css({
-            "marginTop": "-100px"
+            "marginTop": "-58px"
         });
         currentPageSideElement = 3
     } else if (settingsSideBar.includes(file)) {
         $("#selectedSidenavElement").stop().show(0, function() {});
         $("#selectedSidenavElement").css({
             "bottom": "0px",
-            "marginTop": "3px"
+            "marginTop": "10px"
         });
         currentPageSideElement = -1
     };
 
     // hides the text under the side labels
-    $(".sidenavText, .sidenavTextLast").hide(0);
+    // $(".sidenavText, .sidenavTextLast").hide(0);
+
+    // event handler of hovering sidenav
+    $(".sidenav").hover(
+        function() {
+            sidebarExpand();
+        }, function() {
+            sidebarCollapse();
+        }
+    )
 
     // event handler of hovering the side labels (dynamic)
     $(".sidenavElement").hover(
         function() { // when hovering
             // $(this).css("background-color", "rgba(93, 93, 93, 0.25)"); // changes label bg colour
-            $(this).find(".sidenavImages").stop().animate({ // moves label up
-                "margin-top": "14px"
-            }, 125, function() {});
-            $(this).find(".sidenavText").stop().show(150, function() {}); // shows text
+            // $(this).find(".sidenavImages").stop().animate({ // moves label up
+            //     "margin-top": "14px"
+            // }, 125, function() {});
+            // $(this).find(".sidenavText").stop().show(150, function() {}); // shows text
             if ($(this)[0]["id"].split("sidenavElement")[1] != currentPageSideElement) { // checks for not highlighting current page
                 if ($(this)[0]["id"].split("sidenavElement")[1] != -1) { // checks for last element as that requires different change in css properties
-                    $("#highlightedSidenavElement").stop().css({
+                    $("#highlightedSidenavElement").css({
                         "bottom": "",
                         "marginTop": sideElementNumbers[$(this)[0]["id"].split("sidenavElement")[1]]
                     });
                 } else {
-                    $("#highlightedSidenavElement").stop().css({
+                    $("#highlightedSidenavElement").css({
                         "bottom": "0px",
                         "marginTop": "3px"
                     });
                 };
-                $("#highlightedSidenavElement").stop().animate({
-                    "opacity": "1"
-                }, 250); // shows the highlight
+                if (sidebarExpanded == true) {
+                    $("#highlightedSidenavElement").stop().animate({
+                        "marginLeft": "108px",
+                        "opacity": "1"
+                    }, 200);
+                } else {
+                    highlightedSidenavElementOpacity = true
+                };
             };
         }, function() { // when leaving hover
             // $(this).css("background-color", "rgba(30, 30, 30, 0)"); //changes label bg colour
-            $(this).find(".sidenavImages").stop().animate({ // moves label down
-                "margin-top": "25px"
-            }, 125, function() {});
-            $(this).find(".sidenavText").stop().hide(275, function() {}); // hides text
+            // $(this).find(".sidenavImages").stop().animate({ // moves label down
+            //     "margin-top": "25px"
+            // }, 125, function() {});
+            // $(this).find(".sidenavText").stop().hide(275, function() {}); // hides text
             $("#highlightedSidenavElement").stop().animate({
                 "opacity": "0"
-            }, 250);
+            }, 200);
         }
     );
 
